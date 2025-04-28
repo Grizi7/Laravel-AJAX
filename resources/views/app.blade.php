4a1a9b82f2a015767de9c24f25cd37bcf10e0025
Grizi7 <mahmoudzidan7717@std.mans.edu.eg>
2025-04-28 19:49:52 +0300

    Remove Vite script inclusion from app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,6 @@
     <!-- Bootstrap JS Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    @vite('resources/js/app.js')
     <script>
         
         const app = document.getElementById('app');

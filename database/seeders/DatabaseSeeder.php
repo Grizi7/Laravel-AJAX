5a454579c42ada92a8f4cfe43d2091a2b4838d31
Grizi7 <mahmoudzidan7717@std.mans.edu.eg>
2025-04-28 19:53:48 +0300

    Increase category seeding from 10 to 20 in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        Category::factory(10)->create();
+        Category::factory(20)->create();
     }
 }
